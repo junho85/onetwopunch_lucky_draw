2e118e5bc17bac2e0bfa74d22c35250caacc2598
junho85 <junho85@gmail.com>
2019-11-14 19:59:22 +0900

    60 or 61 명 까지 당첨. 이후 100명 까지는 후보

--- lucky_draw_broadcast.py
+++ lucky_draw_broadcast.py
@@ -24,11 +24,14 @@ def load_applies():
 applies = load_applies()
 print(applies)
 
-wins = []
-sum = 0
+wins = [] # 당첨자 목록
+wins2 = [] # 당첨후보 목록
+
+sum = 0 # 총 참여 인원
 max_win = 60 # 동행인 포함 총 인원
-win_count_with_companion = 0
-win_count_without_companion = 0
+
+win_count_with_companion = 0 # 동행인 포함 당첨자수
+win_count_without_companion = 0 # 동행인 미포함 당첨자수
 
 nicknames = []
 
@@ -39,30 +42,41 @@ while True:
     nicknames.append(win_candidate[1])
 
     if win_candidate[2].strip().upper() == "O":
-        if max_win - sum >= 2:
-            sum += 2
-            win_count_with_companion += 1
-        else:
-            print(f"😥 남은 자리가 하나뿐이라 탈락 ㅠㅠ {win_candidate}")
-            continue
+        sum += 2
+        win_count_with_companion += 1
     else:
         sum += 1
         win_count_without_companion += 1
 
-    print(f"🎊 당첨 {win_candidate}")
-    wins.append(win_candidate)
+    if sum <= max_win:
+        # 60명 까지 당첨
+        print(f"🎊 당첨 {win_candidate}")
+        wins.append(win_candidate)
+    else:
+        # 60명 넘으면 후보. 후보는 인원수 100명 까지
+        print(f"후보 {win_candidate}")
+        wins2.append(win_candidate)
 
-    if sum == max_win:
+    # 100명 넘으면 끝
+    if sum >= 100:
         break
 
     time.sleep(0.1)
 
+print()
 print("🥳🎁👏당첨을 축하합니다🎊🎉🎈")
 print(wins)
-with open("당첨결과.txt", "w") as file:
+
+# 당첨자 목록 저장
+with open("당첨결과_당첨자.txt", "w") as file:
     for win in wins:
         file.write(f"{win[0]},{win[1]},{win[2]},\n")
 print(f"당첨자수:{len(wins)}, (동행인 O:{win_count_with_companion}, 동행인 X:{win_count_without_companion}, 총인원:{win_count_with_companion*2 + win_count_without_companion})")
+
+# 당첨자후보 목록 저장
+with open("당첨결과_당첨자후보.txt", "w") as file:
+    for win in wins2:
+        file.write(f"{win[0]},{win[1]},{win[2]},\n")
 
 # 당첨자 중 중복된 닉네임 있는지 검사
 nicknames_counter = collections.Counter(nicknames)
